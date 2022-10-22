1ff81a2b8f34b16bf2fd17fd09ca2acf90672111
Daniel Hayes <djhayes1994@gmail.com>
2022-10-22 08:07:58 -0400

    Added comments to make it more readable.

--- weather.py
+++ weather.py
@@ -2,20 +2,24 @@ import requests
 import json
 import os
 
+# Grabs API key from system variable and also provides base URL.
 apiKey = os.environ.get('openweather_apiKey')
 base_url = "https://api.openweathermap.org/data/2.5/weather?"
 
+# Grabs user input so we know what zip code to use.
 def getZip():
     while True:
         zip = input('Insert zip code: ')
         return zip
 
+# Queries the OpenWeather API to get information on the weather for the supplied zip.
 def getWeather(zipCode = getZip()):
     complete_url = base_url + "zip=" + zipCode + "&appid=" + apiKey
     response = requests.get(complete_url)
     jsonOut = response.json()
     return jsonOut
 
+# Gets the current tempurature for the zip code provided in Kelvin and converts it to Farenheit.
 def getTempF():
     jsonOut = getWeather()
     main = jsonOut['main']
@@ -23,6 +27,7 @@ def getTempF():
     tempConversion = round((currentTemp - 273.15) * 9/5 + 32, 2)
     return tempConversion
 
+# Gets the current feels like tempurature for the zip code provided in Kelvin and converts it to Farenheit.
 def getFeelsLike():
     jsonOut = getWeather()
     main = jsonOut['main']
@@ -30,23 +35,26 @@ def getFeelsLike():
     tempFeelsLikeConversion = round((currentFeelsLike - 273.15) * 9/5 + 32, 2)
     return tempFeelsLikeConversion
 
+# Gets the current humidity for the zip code provided in Kelvin and converts it to Farenheit.
 def getHumidity():
     jsonOut = getWeather()
     main = jsonOut['main']
     currentHumidity = main['humidity']
     return currentHumidity
 
+# Gets the City for the zip code provided in Kelvin and converts it to Farenheit.
 def getCityName():
     jsonOut = getWeather()
     return jsonOut['name']
 
+# Gets the Country for the zip code provided in Kelvin and converts it to Farenheit.
 def getCountryCode():
     jsonOut = getWeather()
     sys = jsonOut['sys']
     countryCode = sys['country']
     return countryCode
 
-
+# Gets the current weather description for the zip code provided in Kelvin and converts it to Farenheit.
 def getWeatherDesc():
     jsonOut = getWeather()
     weather = jsonOut['weather']
